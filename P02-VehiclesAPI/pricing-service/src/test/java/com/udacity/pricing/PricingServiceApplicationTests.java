133a1cf7d82970a249155dd81ece67431043cbcd
HieuVu <hieuvu.cdt95@gmail.com>
2024-04-03 20:34:01 +0700

    finish project

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -2,15 +2,34 @@ package com.udacity.pricing;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.net.URI;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.Matchers.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest()
 public class PricingServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mockMvc;
 
+	@Test
+	public void contextLoad() throws Exception {
+		given()
+				.mockMvc(mockMvc)
+				.when()
+				.get(new URI("/services/price?vehicleId=1"))
+				.then()
+				.statusCode(HttpStatus.OK.value())
+				.body("price", notNullValue())
+				.body("currency", notNullValue())
+				.body("vehicleId", notNullValue());
+	}
 }
--- P02-VehiclesAPI/vehicles-api/src/test/java/com/udacity/vehicles/VehiclesApiApplicationTests.java
+++ P02-VehiclesAPI/vehicles-api/src/test/java/com/udacity/vehicles/VehiclesApiApplicationTests.java
@@ -2,23 +2,21 @@ package com.udacity.vehicles;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.net.URI;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 public class VehiclesApiApplicationTests {
 
     @Test
     public void contextLoads() {
     }
 
-    @Test
-    public void getPriceById() throws Exception {
-        int vehicleId = 1;
-        URI uri = new URI("/services/price?vehicleId=" + vehicleId);
 
-    }
 }
